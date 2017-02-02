081511ebedcf01d62bc5ed952f4a6ca52f33ecdb
Radu Fiser <radu.fiser@endava.com>
2017-02-02 18:13:58 +0200

    Programming assignment 2: Permutations

--- src/main/java/com/rfiser/algorithms/permutation/Permutation.java
+++ src/main/java/com/rfiser/algorithms/permutation/Permutation.java
@@ -1,7 +1,19 @@
 package com.rfiser.algorithms.permutation;
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
     public static void main(String[] args) {
+        final int k = StdIn.readInt();
+        final RandomizedQueue<String> queue = new RandomizedQueue<String>();
 
+        while (!StdIn.isEmpty()) {
+            final String s = StdIn.readString();
+                queue.enqueue(s);
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(queue.dequeue());
+        }
     }
 }
